ecb4a2445b5ffd96eb3e8743f8492127e1658e73
Oleg Legun <oleg.legun@gmail.com>
2017-03-21 01:42:34 +0300

    add dictionary index.js

---
+++ dictionaries/index.js
@@ -0,0 +1,103 @@
+const ru = {
+
+    slider: ['Добро пожаловать', 'на сайт компании', '"ПромXим-Сфера"'],
+
+    company_info: [
+        'ЗАО ПКФ «ПромХим-Сфера» — поставщик технологичного оборудования и услуг для нефтегазовой, авиационной и металлургических отраслей. Качество и сроки поставляемой нами продукции соответствуют самым высоким требованиям наших заказчиков.',
+        'За время работы компании реализовано большое количество проектов по поставке оборудования. В связи с этим, на базе ЗАО ПКФ «ПромХим-Сфера» открыт сервисный центр по технической поддержке эксплуатируемого оборудования и сервисного обслуживания. Мы рассматриваем каждый проект как индивидуальную задачу и прилагаем все усилия для её реализации.'
+    ],
+
+    advantages_title: 'Наши преимущества',
+
+    advantages_list: [
+        { title: 'Большой опыт работы', body: 'Опыт реализации проектов в различных отраслях 10+ лет' },
+        { title: 'Индивидуальный подход', body: 'Индивидуальный подход к каждому заказчику' },
+        { title: 'Слаженная команда', body: 'Мы совершенствуем свои профессиональные навыки' },
+        { title: 'Удобная доставка', body: 'Поставка оборудования и услуг в любую точку России' },
+    ],
+
+    business_lines_title: 'Основные направления деятельности',
+
+    business_lines_list: [
+        { title: ['Поставка', 'оборудования', 'и', 'сервис'], body: 'Комплексное обеспечение оборудованием и инжиниринговыми услугами предприятий различных отраслей промышленности, строительных и эксплуатационных компаний и организаций.', },
+        { title: ['Проведение', 'монтажных', 'и', 'пуско-наладочных работ'], body: 'По желанию заказчика высокопрофессиональные специалисты ЗАО ПКФ «ПромХим-Сфера» осуществляют шефмонтажные и пуско-наладочные работы. Шефмонтажные работы заключается в проверке правильности установки и подключения нашего оборудования, а результатом пуско-наладочных работ является запуск оборудования в эксплуатацию.', },
+        { title: ['', 'Консалтинг', 'и', 'маркетинг'], body: 'Консалтинговые и маркетинговые услуги в области поставок оборудования, подбор нестандартного технологического и аналитического оборудования российского и зарубежного производства.', },
+        { title: ['Обучающие', 'семинары', 'и', 'конференции'], body: 'Организация обучающих семинаров как на площадках заказчика, так и на заводах изготовителях. Обучение проводится по самым актуальным темам и направлениям, являющимися ключевыми для наших заказчиков. Периодически наша компания проводит конференции в рамках нефтегазовых выставок.', }
+    ],
+
+    testimonials_title: 'Отзывы заказчиков',
+
+    testimonials_list: [
+        {
+            body: 'Выражаем благодарность за досрочную поставку партии критического оборудования производства фирмы «Rohrback Cosasco Systems, Inc.», США, выполненную Вашей компанией в самые кратчайшие сроки в рамках программы по ускорению поставок, утвержденной ОАО «ТАНЕКО», ОАО «Татнефть» и Министерством строительства РТ.',
+            caption: '―ОАО «ТАНЕКО»',
+            date: '20/07/2010г.'
+        },
+        {
+            body: 'За период эксплуатации в ОАО «ТНК-Нижневартовск» оборудование мониторинга коррозии показало высокую степень надежности. Случаев отказов в работе или дефектов не отмечено. Все заявленные изготовителем технические характеристики и параметры выдерживаются в полном объеме.',
+            caption: '―ОАО «ТНК-Нижневартовск»',
+            date: '1/03/2011г.'
+        },
+    ],
+
+    questions: {
+        title: 'У вас появились вопросы?',
+        body: 'Наши специалисты будут рады вам помочь!',
+        button: 'Обратная связь'
+    },
+
+    footer_copyright: 'ЗАО ПКФ "ПромХим-Сфера" ©',
+}
+
+const en = {
+
+    slider: ['Welcome', 'to the website of', 'JSC PKF "PromHim-Sfera"'],
+
+    company_info: [
+        'JSK PKF "PromHim-Sfera" is recommended in the Russian market as a reliable partner in the field of complex supplies of industrial equipment. Our company supplies equipment for the oil and gas industry, aviation and metal industry for more than ten years and constantly continues its development. Professional approach to business and gained experience have greatly helped us to expand the range of products and geography of delivery, as well as to succeed in different lines of business – delivery of industrial and laboratory equipment, engineering services and supply of spare parts.',
+        'Since the foundation of the company a lot of equipment supply projects have been executed. In connection with this the service center for technical support of supplied equipment and maintenance service has been opened on the basis of JSC PFK "PromHim-Sfera". We consider each project as an individual task and make every effort for its execution.'
+    ],
+
+    advantages_title: 'Our advantages',
+
+    advantages_list: [
+        { title: 'Great experience', body: 'Experience in execution of projects in wide range of sectors 10+ years' },
+        { title: 'Individual approach', body: 'Custom-tailored service for each client' },
+        { title: 'Coordinated team', body: 'We are constantly developing our professional skills' },
+        { title: 'Convenient delivery', body: 'We deliver equipment and services to anywhere in Russia' },
+    ],
+
+    business_lines_title: 'Main business lines',
+
+    business_lines_list: [
+        { title: ['Equipment', 'supply', 'and', 'service'], body: 'We provide equipment and engineering services for enterprises of different industrial spheres, construction and operation companies and organizations.', },
+        { title: ['Performing of', 'assembling', 'and', 'commissioning works'], body: 'At customer\'s request the highly professional specialists of JSC PKF "PromHim-Sfera" will provide installation supervision and startup and commissioning works. Installation supervision operations involve verification of installation and connection of our equipment and performing of commissioning works results in putting equipment into operation.', },
+        { title: ['', 'Consulting', 'and', 'marketing'], body: 'We provide consulting and marketing services for equipment supply, selection of unique technological and analytical equipment produced by Russian or foreign manufacturers.', },
+        { title: ['Training', 'seminars', 'and', 'conferences'], body: 'We organize training seminars at customer\'s sites as well as at manufacturing plants. Our trainings concern issues and directions which are the most actual for our customers. We also speak at conferences at oil and gas exhibitions.', }
+    ],
+
+    testimonials_title: 'Customers opinions',
+
+    testimonials_list: [
+        {
+            body: 'We would like to express our gratitude for early delivery of critical equipment batch manufactured by "Rohrback Cosasco Systems, Inc.", USA, made by your company within the shortest possible time as part of the supply acceleration program, approved by OJSC "TANECO", OJSC "Tatneft" and the Ministry of Construction of the Republic of Tatarstan.',
+            caption: '―OJSC "TANECO"',
+            date: '20/07/2010'
+        },
+        {
+            body: 'During the operation period in OJSC "TNK-Nizhnevartovsk" the corrosion monitoring equipment supplied by your company showed a high degree of reliability. There were no equipment failures and no defects were found. The technical specification and all the parameters declared by the manufacturer correspond to reality.',
+            caption: '―OJSC "TNK-Nizhnevartovsk"',
+            date: '1/03/2011'
+        },
+    ],
+
+    questions: {
+        title: 'Do you have any questions?',
+        body: 'Our specialists will be glad to help you!',
+        button: 'Feedback'
+    },
+
+    footer_copyright: 'JSC PKF "PromHim-Sfera" ©',
+}
+
+export default { ru, en }
