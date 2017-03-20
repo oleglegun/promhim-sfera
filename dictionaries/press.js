c30744d7da6e6cb72494556443acf60e690b1a25
Oleg Legun <oleg.legun@gmail.com>
2017-03-20 23:05:56 +0300

    add dictionary press.js

---
+++ dictionaries/press.js
@@ -0,0 +1,91 @@
+const ru = {
+    'press': [
+        {
+            'title': 'НЕФТЯНИК',
+            'caption': 'Выпуск №13 | Апрель 2010',
+            'description': 'Коррозия — под контролем',
+        },
+        {
+            'title': 'Территория Нефтегаз',
+            'caption': 'Выпуск №6 | Июнь 2013',
+            'description': 'Наиболее эффективные решения в области коррозионного мониторинга',
+        },
+        {
+            'title': 'Нефтяник Удмуртии',
+            'caption': 'Выпуск №21 | Август 2013',
+            'description': 'Microcor: остановить коррозию',
+        },
+        {
+            'title': 'Инженерная практика',
+            'caption': 'Выпуск №8 | Август 2011',
+            'description': 'Опыт применения антикоррозионной защиты и эксплуатации системы коррозионного мониторинга',
+        },
+        {
+            'title': 'Oil&Gas Eurasia',
+            'caption': 'Выпуск №5 | Май 2014',
+            'description': 'Новые технологии перекачки продукции скважин',
+        },
+        {
+            'title': 'Oil&Gas Eurasia',
+            'caption': 'Выпуск №6 | Июнь 2013',
+            'description': 'Мониторинг коррозии Rohrback Cosasco Systems',
+        },
+        {
+            'title': 'Oil&Gas Eurasia',
+            'caption': 'Выпуск №6 | Июнь 2011',
+            'description': 'Наши эффективные решения для борьбы с коррозией',
+        },
+        {
+            'title': 'Oil&Gas Eurasia',
+            'caption': 'Выпуск №6 | Июнь 2009',
+            'description': 'Мониторинг коррозии в системах высокого давления',
+        },
+    ],
+}
+
+const en = {
+    'press_magazines': [
+        {
+            title: 'NEFTIANIK',
+            caption: 'Issue №13 | April 2010',
+            description: 'Corrosion is under control',
+        },
+        {
+            title: 'Territoria Neftegaz',
+            caption: 'Issue №6 | June 2013',
+            description: 'The most effective corrosion monitoring solutions',
+        },
+        {
+            title: 'Neftianik Udmurtii',
+            caption: 'Issue №21 | August 2013',
+            description: 'Microcor: to stop corrosion',
+        },
+        {
+            title: 'Inzhenernaya praktika',
+            caption: 'Issue №8 | August 2011',
+            description: 'Experience of using anti-corrosion protection and operating corrosion monitoring systems',
+        },
+        {
+            title: 'Oil&Gas Eurasia',
+            caption: 'Issue №5 | May 2014',
+            description: 'New Technology for Pumping Wellbore Fluids',
+        },
+        {
+            title: 'Oil&Gas Eurasia',
+            caption: 'Issue №6 | June 2013',
+            description: 'Rohrback Cosasco Corrosion Monitoring Systems',
+        },
+        {
+            title: 'Oil&Gas Eurasia',
+            caption: 'Issue №6 | June 2011',
+            description: 'Our effective solutions for corrosion protection',
+        },
+        {
+            title: 'Oil&Gas Eurasia',
+            caption: 'Issue №6 | June 2009',
+            description: 'Corrosion monitoring in the high pressure systems',
+        }
+    ],
+}
+
+export default { ru, en }
