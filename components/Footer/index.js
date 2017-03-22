1ca63f4bb0da75dadf232aec0eafbb11d108701c
Oleg Legun <oleg.legun@gmail.com>
2017-03-22 12:57:47 +0300

    update component Footer; add Footer bg images

---
+++ components/Footer/index.js
@@ -0,0 +1,41 @@
+import React, { PropTypes } from 'react'
+import MenuItem from '../../components/MenuItem'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import logoRu from './logo-footer.png'
+import logoEn from './logo-footer.png'
+import footer_dict from '../../dictionaries/footer'
+
+function Footer(props, context) {
+    const dict = footer_dict[context.lang]
+    return (
+        <footer>
+            <div className="bg">
+                <section className="wrapper">
+                    <div className="logo">
+                        <Link to={prefixLink('/')}>
+                            <img src={context.lang === 'ru' ? logoRu : logoEn}/>
+                        </Link>
+                    </div>
+                    <div className="footer-left">
+                        <ul id="footer-menu" className="menu">
+                            <MenuItem path={dict.footer_menu.press.path} title={dict.footer_menu.press.title}/>
+                            <li className="li-sep>"> | </li>
+                            <MenuItem path={dict.footer_menu.contacts.path} title={dict.footer_menu.contacts.title}/>
+                        </ul>
+                        <p className="copyright">{dict.copyright + new Date().getFullYear()}</p>
+                    </div>
+                    <div id="footer-icons"></div>
+                </section>
+            </div>
+        </footer>
+    )
+}
+
+Footer.contextTypes = {
+    lang: PropTypes.string.isRequired
+}
+Footer.propTypes = {}
+Footer.defaultProps = {}
+
+export default Footer
---
+++ dictionaries/footer.js
@@ -0,0 +1,30 @@
+const ru = {
+    footer_menu: {
+        press: {
+            title: 'Пресса о нас',
+            path: '/press/'
+        },
+        contacts: {
+            title: 'Контакты',
+            path: '/contacts/'
+        }
+    },
+    copyright: 'ЗАО ПКФ "ПромХим-Сфера" © ',
+}
+
+const en = {
+    footer_menu: {
+        press: {
+            title: 'Media about us',
+            path: '/press/'
+        },
+        contacts: {
+            title: 'Contacts',
+            path: '/contacts/'
+        }
+    },
+    copyright: 'JSC PKF "PromHim-Sfera" © ',
+}
+
+export default { ru, en }
+
